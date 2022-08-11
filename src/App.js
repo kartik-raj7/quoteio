a1ccdf213b4d498c82c05d8b15b2c9b2f2eb885d
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-08-12 00:22:47 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import {useState,useEffect} from 'react';
 import axios from 'axios';
-import User from './User';
 import {button} from 'bootstrap'
 
 function App(){
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './Home';
-import reportWebVitals from './reportWebVitals';
 import Footer from './footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,8 +11,3 @@ root.render(
     <Footer/>
     </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
